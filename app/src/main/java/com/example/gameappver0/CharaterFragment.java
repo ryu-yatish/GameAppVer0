21340a38c4d2b15cddccfbe0c508e60962a86bc9
ryu-yatish <yatishagrawal2304@gmail.com>
2020-02-19 20:04:53 +0530

    Coins added and some graphic shanges made.

--- app/src/main/java/com/example/gameappver0/CharaterFragment.java
+++ app/src/main/java/com/example/gameappver0/CharaterFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -21,10 +22,15 @@ import android.widget.Toast;
 public class CharaterFragment extends Fragment {
 
     private EditText username;
+    private TextView txt_coin;
     private Button bn_save,bn_clear;
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String USER = "user";
+    public static final String COIN = "coin";
+
     private String username_load;
+    private int coin_load;
+
     public CharaterFragment() {
         // Required empty public constructor
     }
@@ -36,6 +42,8 @@ public class CharaterFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_charater, container, false);
         username = view.findViewById(R.id.txt_username);
+        txt_coin = view.findViewById(R.id.txt_coin);
+
         final Context context = this.getContext();
         updateData(context);
         bn_save = view.findViewById(R.id.bn_save);
@@ -52,6 +60,7 @@ public class CharaterFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 defaultData(context);
+
             }
         });
 
@@ -75,6 +84,7 @@ public class CharaterFragment extends Fragment {
     {
         SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS,Context.MODE_PRIVATE);
         username_load = sharedPreferences.getString(USER, "Guest");
+        coin_load = sharedPreferences.getInt(COIN,0);
     }
 
     public void updateData(Context context)
@@ -82,6 +92,9 @@ public class CharaterFragment extends Fragment {
         SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS,Context.MODE_PRIVATE);
         loadData(context);
         username.setText(username_load);
+        String s=Integer.toString(coin_load);
+        txt_coin.setText(s);
+
     }
 
     public void defaultData(Context context)
@@ -90,8 +103,20 @@ public class CharaterFragment extends Fragment {
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
         editor.putString(USER, "Guest");
+        editor.putInt(COIN,0);
         editor.apply();
         Toast.makeText(context,"default value set",Toast.LENGTH_SHORT);
+        updateData(context);
+    }
+    public void coinIncrease(Context context, int value)
+    {
+        SharedPreferences sharedPreferences =context.getSharedPreferences(SHARED_PREFS,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        int c=sharedPreferences.getInt(COIN,0);
+        c=c+value;
+        editor.putInt(COIN,c);
+        editor.apply();
         updateData(context);
     }
 }
